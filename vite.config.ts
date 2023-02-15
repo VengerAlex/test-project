948545c1a38db5acf817ec6cbb498dd204bc7087
VengerAlex <alexvenger90@gmail.com>
2023-02-15 15:49:31 +0200

    add path to data folder

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@utils': path.resolve(__dirname, './src/utils'),
       '@pages': path.resolve(__dirname, './src/pages'),
+      '@data': path.resolve(__dirname, './src/data'),
     }
   },
 })
