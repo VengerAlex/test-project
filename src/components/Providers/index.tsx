e43cf280edf3d98233ceca57ae6d02bdb9904ecc
VengerAlex <alexvenger90@gmail.com>
2023-02-15 18:43:57 +0200

    add QueryClientProvider to MainProvider

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -2,15 +2,20 @@ import {FC, PropsWithChildren} from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import MainLayout from '@components/MainLayout'
 import {ThemeProvider} from '@mui/material'
+import {QueryClient, QueryClientProvider} from 'react-query'
 import {theme} from '../../themes'
+
+const queryClient = new QueryClient()
 
 const MainProvider: FC<PropsWithChildren> = ({children}) => (
   <BrowserRouter>
-    <MainLayout>
-      <ThemeProvider theme={theme}>
-        {children}
-      </ThemeProvider>
-    </MainLayout>
+    <QueryClientProvider client={queryClient}>
+      <MainLayout>
+        <ThemeProvider theme={theme}>
+          {children}
+        </ThemeProvider>
+      </MainLayout>
+    </QueryClientProvider>
   </BrowserRouter>
 )
 
