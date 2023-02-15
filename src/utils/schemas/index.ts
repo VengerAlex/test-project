e97f1bf6f944ec86e71415ff54d925e493fd53a1
VengerAlex <alexvenger90@gmail.com>
2023-02-15 17:09:15 +0200

    create schema for SignIn form

---
+++ src/utils/schemas/index.ts
@@ -0,0 +1,18 @@
+import * as yup from 'yup'
+
+const SPACE_REGEXP = /^(.*)?\S+(.*)?$/
+const SPACE_WARNING = 'can\'t be empty'
+
+export const signInSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required()
+    .min(5)
+    .max(30),
+  password: yup
+    .string()
+    .matches(SPACE_REGEXP, SPACE_WARNING)
+    .required()
+    .min(8)
+    .max(30),
+})
