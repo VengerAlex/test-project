ac55ef0c17f69b769e79394906ef105e3b5734a5
VengerAlex <alexvenger90@gmail.com>
2023-02-15 14:50:44 +0200

    change the route of page

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -2,7 +2,7 @@ export const ROUTES = {
   HOME: '/',
   SIGN_IN: '/sign-in',
   COMPANY_ABOUT: '/about/company',
-  TEAM_ABOUT: '/about/team',
+  TEAM_ABOUT: '/about/main-team',
   SETTINGS: '/settings',
   NotFound: '*',
 }
