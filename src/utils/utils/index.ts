35c8ba85590922fe2bd3b00413a256a1902a0cbe
VengerAlex <alexvenger90@gmail.com>
2023-02-15 17:09:50 +0200

    add helper for displaying error msg

---
+++ src/utils/utils/index.ts
@@ -0,0 +1,9 @@
+// TODO! change any
+
+export const showErrorText = (
+  errors: any,
+  value: string,
+  fieldValue?: string,
+) => {
+  return errors[value]?.message && !!fieldValue ? errors[value]?.message : ' '
+}
