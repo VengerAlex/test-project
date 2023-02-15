d26de282d3e84afc74ca4f294ee6a58e7d10db0c
VengerAlex <alexvenger90@gmail.com>
2023-02-15 13:58:30 +0200

    wrap our root element with MainProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {App} from "./App";
+import MainProvider from '@components/Providers'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+  .render(<MainProvider><App /></MainProvider>)
