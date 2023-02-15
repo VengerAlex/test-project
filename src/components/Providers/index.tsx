977acfd4d1ead4eca7d5f4d356b3e56d7a65f667
VengerAlex <alexvenger90@gmail.com>
2023-02-15 13:56:50 +0200

    add custom theme to ThemProvider
    it's just a simple customization for headings,
    in future it's possible to add or
    create your custom theme colors or classes
    by extending your current theme

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,11 +1,15 @@
 import {FC, PropsWithChildren} from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import MainLayout from '@components/MainLayout'
+import {ThemeProvider} from '@mui/material'
+import {theme} from '../../themes'
 
 const MainProvider: FC<PropsWithChildren> = ({children}) => (
   <BrowserRouter>
     <MainLayout>
-      {children}
+      <ThemeProvider theme={theme}>
+        {children}
+      </ThemeProvider>
     </MainLayout>
   </BrowserRouter>
 )
