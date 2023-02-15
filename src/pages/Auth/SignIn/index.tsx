0fc1f85997b35218c305d35b84d41b38d1d46d9a
VengerAlex <alexvenger90@gmail.com>
2023-02-15 17:11:53 +0200

    add form to SignIn page

--- src/pages/Auth/SignIn/index.tsx
+++ src/pages/Auth/SignIn/index.tsx
@@ -1,7 +1,83 @@
-import {Typography} from '@mui/material'
+import {
+  Box, Button, Stack, Typography
+} from '@mui/material'
+import { yupResolver } from '@hookform/resolvers/yup'
+import {ISignInForm} from '@utils/types'
+import {useForm} from 'react-hook-form'
+import {signInSchema} from '@utils/schemas'
+import {Input} from '@ui/Input'
+import {showErrorText} from '@utils/utils'
 
 const SignIn = () => {
-  return <Typography>Sign In</Typography>
+  const {
+    control,
+    getValues,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<ISignInForm>({
+    mode: 'onChange',
+    resolver: yupResolver(signInSchema),
+  })
+  const { username, password } = getValues()
+
+  const submitHandler = (data: ISignInForm) => {
+    const { username, password } = data
+
+    console.log(username, password)
+  }
+
+  const cancelRequestHandler = () => {
+    console.log('CANCEl')
+  }
+
+  return <Box sx={{maxWidth: '500px'}}>
+    <Typography variant='h1' mb={2}>
+      Sign In
+    </Typography>
+    <form onSubmit={handleSubmit(submitHandler)}>
+      <Stack
+        direction='column'
+        alignItems='center'
+        spacing={2}
+      >
+        <Input
+          fullWidth
+          helperText={showErrorText(errors, 'username', username)}
+          autoFocus
+          placeholder='Example123'
+          error={!!errors.username && !!username}
+          control={control}
+          formName='username'
+          label='Username'
+        />
+        <Input
+          fullWidth
+          helperText={showErrorText(errors, 'password', password)}
+          error={!!errors.password && !!password}
+          sx={{ mb: 2 }}
+          placeholder='***************'
+          control={control}
+          formName='password'
+          label='Password'
+          isPassword
+        />
+      </Stack>
+      <Button
+        sx={{mr: 2}}
+        variant='contained'
+        type='submit'
+        disabled={!isValid}
+      >
+        Log In
+      </Button>
+      <Button
+        variant='contained'
+        onClick={cancelRequestHandler}
+      >
+        Cancel
+      </Button>
+    </form>
+  </Box>
 }
 
 export default SignIn
