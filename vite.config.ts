17a67a252e65114f06b7839dc6fafb96fc0f7e64
VengerAlex <alexvenger90@gmail.com>
2023-02-15 10:56:57 +0200

    set up paths for files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.ts', '.tsx'],
+    alias: {
+      '@ui': path.resolve(__dirname, './src/components/ui'),
+      '@components': path.resolve(__dirname, './src/components'),
+    }
+  },
 })
