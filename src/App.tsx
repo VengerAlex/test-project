c8b10c3cc0dd1ecd1c00ea30d5cf2d4c0348bf07
VengerAlex <alexvenger90@gmail.com>
2023-02-15 14:02:40 +0200

    add routes for our pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
-export const App = () => {
-  return <h1>Start</h1>
+import {Route, Routes} from 'react-router-dom'
+import Home from '@pages/Home'
+import Team from '@pages/About/Team'
+import Company from '@pages/About/Company'
+import SignIn from '@pages/Auth/SignIn'
+import NotFound from '@pages/NotFound'
+import {ROUTES} from '@utils/constants'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.TEAM_ABOUT} element={<Team />} />
+      <Route path={ROUTES.COMPANY_ABOUT} element={<Company />} />
+      <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+      <Route path={ROUTES.NotFound} element={<NotFound />} />
+    </Routes>
+  )
 }
+
+export default App
