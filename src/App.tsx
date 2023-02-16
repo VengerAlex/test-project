ac474bc3b7907a1832eb2e579cc58296db06ecd8
VengerAlex <alexvenger90@gmail.com>
2023-02-16 18:05:30 +0200

    divide our routes in private & public

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import {
   Home, Team, SignIn,
   Company, NotFound
 } from '@pages/index'
+import {PrivateRoute} from '@components/routes/PrivateRoute'
+import {PublicRoute} from '@components/routes/PublicRoute'
 
 const App = () => {
   return (
     <Routes>
-      <Route path={ROUTES.HOME} element={<Home />} />
-      <Route path={ROUTES.TEAM_ABOUT} element={<Team />} />
-      <Route path={ROUTES.COMPANY_ABOUT} element={<Company />} />
-      <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+      <Route element={<PrivateRoute />}>
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.TEAM_ABOUT} element={<Team />} />
+        <Route path={ROUTES.COMPANY_ABOUT} element={<Company />} />
+      </Route>
+      <Route element={<PublicRoute />}>
+        <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+      </Route>
       <Route path={ROUTES.NotFound} element={<NotFound />} />
     </Routes>
   )
