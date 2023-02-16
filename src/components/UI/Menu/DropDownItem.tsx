4fea3aebe9ab8c0aae8f2bd4f263dc4d5e23ac58
VengerAlex <alexvenger90@gmail.com>
2023-02-16 18:07:10 +0200

    update DropDownItem

--- src/components/UI/Menu/DropDownItem.tsx
+++ src/components/UI/Menu/DropDownItem.tsx
@@ -1,35 +1,43 @@
-import {FC, useState} from 'react'
-import {Box, Typography} from '@mui/material'
-import {DropDown} from '@ui/styles'
-import {Link} from 'react-router-dom'
+import {useState} from 'react'
+import {
+  Box, Typography, ListItem, ListItemIcon, ListItemButton
+} from '@mui/material'
+import {Link, useLocation} from 'react-router-dom'
 import {MenuItemTS} from '@utils/types'
+import { CaretUp, CaretDown } from 'phosphor-react'
 
 interface DropDownItemProps {
   item: MenuItemTS
 }
 
-export const DropDownItem: FC<DropDownItemProps> = ({item}) => {
+export const DropDownItem = ({item}: DropDownItemProps) => {
+  const {pathname} = useLocation()
   const [isDropDownOpen, setIsDropDownOpen] = useState(false)
+
+  const currentIcon = isDropDownOpen ? <CaretUp size={32} /> : <CaretDown size={32} />
+  const isActivePage = pathname === item.href
 
   if (item.items) {
     return (
-      <Box sx={{display: 'block'}} >
-        <Box>
-          <Typography  onClick={() => setIsDropDownOpen(!isDropDownOpen)}>
-            {item.label}
-          </Typography>
-        </Box>
-        <DropDown sx={{display: isDropDownOpen ? 'block' : 'none'}}>
-          {item.items.map((child: MenuItemTS, index: number) =>
-            <DropDownItem key={index} item={child} />)
-          }
-        </DropDown>
+      <Box>
+        <ListItemButton
+          onClick={() => setIsDropDownOpen(!isDropDownOpen)}>
+          {item.label}
+          <ListItemIcon>
+            {currentIcon}
+          </ListItemIcon>
+        </ListItemButton>
+        {isDropDownOpen && item.items.map((child, index) =>
+          <ListItem key={index}>
+            <DropDownItem item={child} />
+          </ListItem>
+        )}
       </Box>
     )
   } else {
     return (
       <Link to={item.href || '#'}>
-        <Typography>{item.label}</Typography>
+        <Typography sx={{color: isActivePage ? 'red' : ''}}>{item.label}</Typography>
       </Link>
     )
   }
