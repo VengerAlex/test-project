eac803ea1c81271a650bde5a1bf2b1ae6b73b473
VengerAlex <alexvenger90@gmail.com>
2023-02-16 18:06:17 +0200

    delete unused styles for Menu

--- src/components/UI/styles.ts
+++ src/components/UI/styles.ts
@@ -2,16 +2,12 @@ import {Box, MenuList, styled} from '@mui/material'
 
 export const MenuWrapper = styled(Box)`
   display: flex;
+  flex-direction: column;
   align-items: center;
   padding: 20px;
 `
 
 export const SMenuList = styled(MenuList)`
-  display: flex;
-  align-items: center;
-`
-
-export const DropDown = styled(MenuList)`
   display: flex;
   flex-direction: column;
   align-items: center;
