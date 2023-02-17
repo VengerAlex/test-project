1c432b9326893a501e013bf143c514d580b54273
VengerAlex <alexvenger90@gmail.com>
2023-02-17 15:42:58 +0200

    restyled login form

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -1,24 +1,19 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import {
-  IconButton,
-  InputAdornment, TextField,
-  TextFieldProps, Typography,
+  IconButton, InputAdornment, TextField, TextFieldProps
 } from '@mui/material'
 import { Eye, EyeSlash } from 'phosphor-react'
 
 // TODO! Replace any
 type InputProps = TextFieldProps & {
-  isPassword?: boolean;
-  isResetPassword?: boolean;
-  control: any;
-  formName: string;
-};
+  isPassword?: boolean
+  isResetPassword?: boolean
+  control: any
+  formName: string
+}
 
 export const Input = ({
-  isPassword = false,
-  formName,
-  control,
-  ...props
+  isPassword = false, formName, control, ...props
 }: InputProps) => {
   const [showPassword, setShowPassword] = useState(false)
 
@@ -26,17 +21,31 @@ export const Input = ({
     <TextField
       {...control.register(formName)}
       {...props}
-      InputLabelProps={{shrink: true}}
       name={formName}
       type={!isPassword ? 'text' : showPassword ? 'text' : 'password'}
-      variant='standard'
+      variant='outlined'
+      size='small'
+      sx={{
+        label: {
+          color: 'dimgray',
+          fontSize: '16px',
+        },
+        input: {
+          color: 'dimgray',
+          fontSize: '16px',
+        },
+        '& .MuiFormHelperText-root': {
+          margin: 0,
+          fontSize: '12px',
+        },
+      }}
       InputProps={
         isPassword
           ? {
             endAdornment: (
               <InputAdornment position='end'>
                 <IconButton onClick={() => setShowPassword(!showPassword)}>
-                  <Typography>{showPassword ? <Eye size={32} /> : <EyeSlash size={32} />}</Typography>
+                  {showPassword ? <Eye size={24} /> : <EyeSlash size={24} />}
                 </IconButton>
               </InputAdornment>
             ),
--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -5,19 +5,18 @@ import { useForm } from 'react-hook-form'
 import { signInSchema } from '@utils/schemas'
 import { Input } from '@ui/Input'
 import { showErrorText } from '@utils/utils'
-import { useLogin } from '../../../services/auth.service'
+import { useLogin } from '../../services/auth.service'
 import { useContext } from 'react'
 import { AuthContext } from '@components/Contexts/AuthContext'
 import { useNavigate } from 'react-router-dom'
 import { ROUTES } from '@utils/constants'
 
 const SignWrapper = styled(Box)`
-  width: 400px;
+  width: 350px;
   background-color: #fff;
-  padding: 20px;
-  border-radius: 20px;
+  padding: 20px 30px;
+  border-radius: 6px;
 `
-
 const SignContainer = styled(Box)`
   background-color: #0775b4;
   display: flex;
@@ -26,6 +25,45 @@ const SignContainer = styled(Box)`
   height: 100vh;
   width: 100vw;
   text-align: center;
+`
+const Title = styled(Typography)`
+  padding: 60px 0 30px;
+  font-weight: bold;
+  color: dimgray;
+  border-bottom: 1px solid lightgray;
+`
+const Subtitle = styled(Typography)`
+  padding: 30px;
+  color: dimgray;
+`
+const PrimaryButton = styled(Button)`
+  width: 100%;
+  font-size: 14px;
+  padding: 7px 10px;
+  text-transform: none;
+  color: white;
+  background-color: #0775b4;
+
+  :disabled {
+    color: white;
+    opacity: 0.6;
+  }
+
+  :hover {
+    color: white;
+    background-color: #1976d2;;
+  }
+`
+const SecondaryButton = styled(PrimaryButton)`
+  background-color: white;
+  color: #0775b4;
+  padding: 7px 10px;
+
+  :hover {
+    background-color: white;
+    color: #1976d2;
+    border-color: #1976d2;
+  }
 `
 
 const SignIn = () => {
@@ -43,6 +81,7 @@ const SignIn = () => {
     mode: 'onChange',
     resolver: yupResolver(signInSchema),
   })
+
   const { username, password } = getValues()
 
   const submitHandler = (data: ISignInForm) => {
@@ -53,9 +92,7 @@ const SignIn = () => {
       {
         onSuccess: ({ data }) => {
           login({ username: data.username, email: data.email })
-
           localStorage.setItem('token', data.token)
-
           navigate(ROUTES.HOME)
         },
         onError: () => {
@@ -69,31 +106,19 @@ const SignIn = () => {
     <SignContainer>
       <SignWrapper>
         <Box>
-          <Typography
-            variant='h3'
-            mb={2}
-            sx={{ color: '#000' }}
-          >
-            Hello System
-          </Typography>
-          <Typography
-            variant='h5'
-            mb={2}
-          >
-            Log In
-          </Typography>
+          <Title variant='h4'>Hello System</Title>
+          <Subtitle variant='h5'>Log In</Subtitle>
         </Box>
+
         <form onSubmit={handleSubmit(submitHandler)}>
           <Stack
             direction='column'
             alignItems='center'
-            spacing={2}
           >
             <Input
               fullWidth
               helperText={showErrorText(errors, 'username', username)}
               autoFocus
-              placeholder='Example123'
               error={!!errors.username && !!username}
               control={control}
               formName='username'
@@ -103,23 +128,29 @@ const SignIn = () => {
               fullWidth
               helperText={showErrorText(errors, 'password', password)}
               error={!!errors.password && !!password}
-              sx={{ mb: 2 }}
-              placeholder='***************'
               control={control}
               formName='password'
               label='Password'
               isPassword
             />
           </Stack>
-          <Button
-            sx={{ mr: 2 }}
-            variant='contained'
+
+          <PrimaryButton
+            variant='outlined'
             type='submit'
             disabled={!isValid}
+            sx={{ mb: 2 }}
           >
             Log In
-          </Button>
+          </PrimaryButton>
+          <SecondaryButton
+            variant='outlined'
+            onClick={() => navigate(-1)}
+          >
+            Cancel
+          </SecondaryButton>
         </form>
+
         {error && (
           <Typography
             variant='h6'
