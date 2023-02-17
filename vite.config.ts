9ffa9ecce59ea213ca1f67076bba10c64914e066
VengerAlex <alexvenger90@gmail.com>
2023-02-17 23:46:25 +0200

    setup tests config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
+import type { InlineConfig } from 'vitest'
+import type { UserConfig } from 'vite'
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ fastRefresh: false })],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+  },
   resolve: {
     extensions: ['.ts', '.tsx'],
     alias: {
@@ -13,6 +25,7 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, './src/pages'),
       '@data': path.resolve(__dirname, './src/data'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
-    }
+      '@tests': path.resolve(__dirname, './src/tests'),
+    },
   },
-})
+} as VitestConfigExport)
